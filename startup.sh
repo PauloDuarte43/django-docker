3ec816237dac4d88f47eb4b37b248badd5ec30c8
Paulo Roger Duarte Antunes <pauloduarte@cwi.com.br>
2019-08-02 18:54:04 -0300

    update startup script

--- startup.sh
+++ startup.sh
@@ -11,22 +11,22 @@ echo "##### EXECUTANDO O COLLECTSTATIC ######"
 echo "#######################################"
 python /app/manage.py collectstatic --noinput
 
-#sleep 4 
-#echo "##################################"
-#echo "##### CRIANDO USUARIO DJANGO #####"
-#echo "##################################"
-#script="
-#from django.contrib.auth.models import User;
-#username = 'admin';
-#password = 'admin';
-#email = 'admin@teste.com';
-#if User.objects.filter(username=username).count()==0:
-#    User.objects.create_superuser(username, email, password);
-#    print('Admin criado!.');
-#else:
-#    print('Admin ja exite!');
-#"
-#printf "$script" | python celery_portal/manage.py shell
+sleep 4 
+echo "##################################"
+echo "##### CRIANDO USUARIO DJANGO #####"
+echo "##################################"
+script="
+from django.contrib.auth.models import User;
+username = 'admin';
+password = 'admin';
+email = 'admin@teste.com';
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Admin criado!.');
+else:
+    print('Admin ja exite!');
+"
+printf "$script" | python /app/manage.py shell
 
 echo "##################################"
 echo "##### INICIANDO O WSGI #####"
